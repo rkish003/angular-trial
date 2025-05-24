3914d693736a8754eea749912ac33dcb5a3df355
Supraja <suprajav78@gmail.com>
2025-05-24 15:19:12 +0530

    weather api

---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { importProvidersFrom, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+apiKey = 'c2e7daa8762790ac82cba7cc08a5354b';
+    
+  constructor(private http:HttpClient) { 
+  }
+
+  getWeather(city:String){
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q={city}&appid={this.apiKey}`)
+  }
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../service/weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent {
-
+  city!:String;
+  weatherData:any;
+  constructor(private ws:WeatherService){}  
+  getWeather(){
+    this.ws.getWeather(this.city)
+    .subscribe(data => {
+        this.weatherData = data;
+        console.log(data);
+    })
+  }
 }
